268b343b0eda47ea4f63d054e4bc3a679a433d21
Yiliang Chen <yiliangchen0113@gmail.com>
2020-09-27 17:28:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-!!text.scan(/^[A-Z].+[\.!?]$/)
+ !!text.match(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
