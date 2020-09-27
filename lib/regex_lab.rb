35b6747a4f765f2b8560c9656c9e2e7488f63ce2
Yiliang Chen <yiliangchen0113@gmail.com>
2020-09-27 17:08:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(//)
 end
 
 def words_five_letters_long(text)
